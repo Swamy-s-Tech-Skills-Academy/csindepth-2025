96292b7c35b07c4a57a44b5372743a61ef2b5d0c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-01-10 16:20:57 +0530

    Create NamesWithStringCollection.cs

---
+++ src/Ch02/Demos/NamesWithStringCollection.cs
@@ -0,0 +1,29 @@
+﻿using Ch02.Interfaces;
+using System.Collections.Specialized;
+
+namespace Ch02.Demos;
+
+public class NamesWithStringCollection : IDemoProgram
+{
+    static StringCollection GenerateNames()
+    {
+        StringCollection names = ["Gamma", "Vlissides", "Johnson", "Helm"];
+
+        return names;
+    }
+
+    static void PrintNames(StringCollection names)
+    {
+        foreach (string name in names!)
+        {
+            Console.WriteLine(name);
+        }
+    }
+
+    public void Run()
+    {
+        StringCollection names = GenerateNames();
+
+        PrintNames(names);
+    }
+}
